4b6bc98a969bfd6e21b140d15d2b8ec1d02a7c2d
Himanshu Yadav <himanshu.yadav@kelltontech.com>
2019-10-24 00:09:25 +0530

    text field functions

--- FirstiOSApp/ViewController.swift
+++ FirstiOSApp/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     //MARK: Properties
     
     @IBOutlet weak var fieldLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        fieldLabel.text = textField.text
+    }
+    
+    //MARK: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         nameTextField.text = "Default Text"
     }
     override func viewDidLoad() {
       
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameTextField.delegate = self
     }
 
 
